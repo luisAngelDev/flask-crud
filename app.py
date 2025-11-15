b952a7333f6546018e290e7a64ee6a3f0a37dbc3
luisAngelDev <luisangel.sde@gmail.com>
2025-11-14 21:45:24 -0500

    anulando servicios basicos

--- app.py
+++ app.py
@@ -39,51 +39,51 @@ def guardar_datos(datos):
         json.dump(datos, f, indent=4)
 
 
-@app.route("/usuarios", methods=["GET"])
-def obtener_usuarios():
-    return jsonify(leer_datos())
+# @app.route("/usuarios", methods=["GET"])
+# def obtener_usuarios():
+#     return jsonify(leer_datos())
 
 
 
-@app.route("/usuarios", methods=["POST"])
-def crear_usuario():
-    usuarios = leer_datos()
-    nuevo = request.get_json()
+# @app.route("/usuarios", methods=["POST"])
+# def crear_usuario():
+#     usuarios = leer_datos()
+#     nuevo = request.get_json()
 
-    if not nuevo.get("nombre") or not nuevo.get("email"):
-        return jsonify({"error": "Faltan campos"}), 400
+#     if not nuevo.get("nombre") or not nuevo.get("email"):
+#         return jsonify({"error": "Faltan campos"}), 400
 
-    nuevo["id"] = max([u["id"] for u in usuarios], default=0) + 1
-    usuarios.append(nuevo)
-    guardar_datos(usuarios)
+#     nuevo["id"] = max([u["id"] for u in usuarios], default=0) + 1
+#     usuarios.append(nuevo)
+#     guardar_datos(usuarios)
 
-    return jsonify(nuevo), 201
-
-
-@app.route("/usuarios/<int:usuario_id>", methods=["PUT"])
-def actualizar_usuario(usuario_id):
-    usuarios = leer_datos()
-    datos_actualizados = request.get_json()
-
-    for u in usuarios:
-        if u["id"] == usuario_id:
-            u.update(datos_actualizados)
-            guardar_datos(usuarios)
-            return jsonify(u)
-
-    return jsonify({"error": "Usuario no encontrado"}), 404
+#     return jsonify(nuevo), 201
 
 
-@app.route("/usuarios/<int:usuario_id>", methods=["DELETE"])
-def eliminar_usuario(usuario_id):
-    usuarios = leer_datos()
-    usuarios_filtrados = [u for u in usuarios if u["id"] != usuario_id]
+# @app.route("/usuarios/<int:usuario_id>", methods=["PUT"])
+# def actualizar_usuario(usuario_id):
+#     usuarios = leer_datos()
+#     datos_actualizados = request.get_json()
 
-    if len(usuarios_filtrados) == len(usuarios):
-        return jsonify({"error": "Usuario no encontrado"}), 404
+#     for u in usuarios:
+#         if u["id"] == usuario_id:
+#             u.update(datos_actualizados)
+#             guardar_datos(usuarios)
+#             return jsonify(u)
 
-    guardar_datos(usuarios_filtrados)
-    return jsonify({"mensaje": "Usuario eliminado"})
+#     return jsonify({"error": "Usuario no encontrado"}), 404
+
+
+# @app.route("/usuarios/<int:usuario_id>", methods=["DELETE"])
+# def eliminar_usuario(usuario_id):
+#     usuarios = leer_datos()
+#     usuarios_filtrados = [u for u in usuarios if u["id"] != usuario_id]
+
+#     if len(usuarios_filtrados) == len(usuarios):
+#         return jsonify({"error": "Usuario no encontrado"}), 404
+
+#     guardar_datos(usuarios_filtrados)
+#     return jsonify({"mensaje": "Usuario eliminado"})
 
 
 
