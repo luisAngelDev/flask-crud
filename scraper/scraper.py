4e1a0892250d28ae9c4b452799446ad0905705be
luisAngelDev <luisangel.sde@gmail.com>
2025-11-27 23:31:49 -0500

    preparando scraping

---
+++ scraper/scraper.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+def obtener_datos():
+    """
+   aqui ira mi scraper para regresar un json
+    """
+    datos = {
+        "esto lueog lo modifico para regresar datos."
+    }
+    return datos
