c994c46839b1db65b46d5dac3cae9cd444a7ebaa
luisAngelDev <luisangel.sde@gmail.com>
2025-12-21 23:25:42 -0500

    scraper inicial minsa

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,48 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
-
-def obtener_datos():
-    """
-
-    """
-
-    url = "https://ejemplo.com"
-
-    try:
-        # 1. Hacer la petición
-        response = requests.get(url, timeout=10)
-
-        # 2. Validar estado
-        response.raise_for_status()
-
-    except requests.exceptions.RequestException as e:
-        # Si falla la conexión
-        return {
-            "error": "No se pudo obtener la página",
-            "detalles": str(e)
-        }
-
-    # 3. Parsear HTML
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # 4. Extraer datos
-    #    Aquí solo dejo un ejemplo genérico
-    titulo = soup.title.text.strip() if soup.title else None
-
-    # 5. Construir la respuesta JSON
-    datos = {
-        "titulo_pagina": titulo,
-        "mensaje": "Scraping funcionando. Aquí irán los datos reales."
-    }
-
-    return datos
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 
 URL = "https://opm-digemid.minsa.gob.pe/#/consulta-producto"
 
 
-def scrape_medicamento(nombre_producto: str):
+def scraper_medicamento(nombre_producto: str):
+
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")        # sin abrir navegador
     options.add_argument("--disable-gpu")
