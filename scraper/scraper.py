1fa0768ab08a13175385d00cf80325932bff8e7c
luisAngelDev <luisangel.sde@gmail.com>
2026-01-05 23:49:38 -0500

    inicializar variables y navegador

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -11,7 +11,26 @@ import time
 URL = "https://opm-digemid.minsa.gob.pe/#/consulta-producto"
 
 
-def obtener_medicamento(nombre_producto: str):
+def obtener_medicamento(
+        producto: str,
+        departamento_value: str = "15",  # ejemplo: Lima
+        provincia_value: str = None,
+        distrito_value: str = None 
+        ):
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")              # sin abrir navegador
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
+    resultados = []
 
 
 
